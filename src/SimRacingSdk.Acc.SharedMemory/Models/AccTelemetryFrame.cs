87c3891bf9f2934836d1b14ed26e7645964da963
Mike Hanson <mike@mikehanson.com>
2025-08-14 07:44:55 +0100

    Mapped all telemetry values to telemetry frame

--- src/SimRacingSdk.Acc.SharedMemory/Models/AccTelemetryFrame.cs
+++ src/SimRacingSdk.Acc.SharedMemory/Models/AccTelemetryFrame.cs
@@ -5,24 +5,48 @@ namespace SimRacingSdk.Acc.SharedMemory.Models;
 public class AccTelemetryFrame
 {
     public AccTelemetryFrame(StaticData staticData,
-                             GraphicsData graphicsData,
-                             PhysicsData physicsData,
-                             int actualSectorIndex)
+        GraphicsData graphicsData,
+        PhysicsData physicsData,
+        int actualSectorIndex)
     {
         this.Abs = physicsData.Abs;
         this.Accelerator = physicsData.Accelerator;
         this.Brake = physicsData.Brake;
+        this.BrakeBias = physicsData.BrakeBias;
+        this.BrakePressureFl = physicsData.BrakePressure[0];
+        this.BrakePressureFr = physicsData.BrakePressure[1];
+        this.BrakePressureRl = physicsData.BrakePressure[2];
+        this.BrakePressureRr = physicsData.BrakePressure[3];
         this.BrakeTempFl = physicsData.BrakeTemperature[0];
         this.BrakeTempFr = physicsData.BrakeTemperature[1];
         this.BrakeTempRl = physicsData.BrakeTemperature[2];
         this.BrakeTempRr = physicsData.BrakeTemperature[3];
         this.CarId = staticData.CarModel;
+        this.Clutch = physicsData.Clutch;
+        this.DiscLifeFl = physicsData.DiscLife[0];
+        this.DiscLifeFr = physicsData.DiscLife[1];
+        this.DiscLifeRl = physicsData.DiscLife[2];
+        this.DiscLifeRr = physicsData.DiscLife[3];
         this.DriverDisplayName = $"{staticData.PlayerName[..1]}. {staticData.PlayerSurname}";
-        this.DriverDisplayName = $"{staticData.PlayerName} {staticData.PlayerSurname}";
+        this.DriverFullName = $"{staticData.PlayerName} {staticData.PlayerSurname}";
+        this.FrontBrakeCompound = physicsData.FrontBrakeCompound;
         this.Fuel = physicsData.Fuel;
         this.Gear = physicsData.Gear;
+        this.Heading = physicsData.Heading;
+        this.IsEngineRunning = physicsData.IsEngineRunning;
+        this.IsIgnitionOn = physicsData.IgnitionOn;
         this.IsInvalid = !graphicsData.IsValidLap;
+        this.IsPitLimiterOn = physicsData.PitLimiterOn;
         this.LapTimeMs = graphicsData.CurrentTimeMs;
+        this.LocationX = physicsData.TyreContactPoint[0].X;
+        this.LocationY = physicsData.TyreContactPoint[0].Z;
+        this.PadLifeFl = physicsData.PadLife[0];
+        this.PadLifeFr = physicsData.PadLife[1];
+        this.PadLifeRl = physicsData.PadLife[2];
+        this.PadLifeRr = physicsData.PadLife[3];
+        this.Pitch = physicsData.Pitch;
+        this.RearBrakeCompound = physicsData.RearBrakeCompound;
+        this.Roll = physicsData.Roll;
         this.Rpm = physicsData.Rpm;
         this.NormalisedCarPosition = graphicsData.NormalizedCarPosition;
         this.SectorIndex = actualSectorIndex;
@@ -39,6 +63,8 @@ public class AccTelemetryFrame
         this.SteeringAngle = physicsData.SteerAngle;
         this.TrackId = staticData.Track;
         this.TimeStamp = DateTime.UtcNow;
+        this.TractionControl = physicsData.TractionControl;
+        this.TurboBoost = physicsData.TurboBoost;
         this.TyreCoreTempFl = physicsData.TyreCoreTemperature[0];
         this.TyreCoreTempFr = physicsData.TyreCoreTemperature[1];
         this.TyreCoreTempRl = physicsData.TyreCoreTemperature[2];
@@ -51,29 +77,52 @@ public class AccTelemetryFrame
         this.TyreTempFr = physicsData.TyreTemp[1];
         this.TyreTempRl = physicsData.TyreTemp[2];
         this.TyreTempRr = physicsData.TyreTemp[3];
-        this.LocationX = physicsData.TyreContactPoint[0]
-                                    .X;
-        this.LocationY = physicsData.TyreContactPoint[0]
-                                    .Z;
-        this.IsEngineRunning = physicsData.IsEngineRunning;
-        this.TractionControl = physicsData.TractionControl;
+        this.WaterTemp = physicsData.WaterTemp;
+        this.WheelSlipFl = physicsData.WheelSlip[0];
+        this.WheelSlipFr = physicsData.WheelSlip[1];
+        this.WheelSlipRl = physicsData.WheelSlip[2];
+        this.WheelSlipRr = physicsData.WheelSlip[3];
     }
 
     public float Abs { get; }
     public float Accelerator { get; }
     public float Brake { get; }
+    public float BrakeBias { get; }
+    public float BrakePressureFl { get; }
+    public float BrakePressureFr { get; }
+    public float BrakePressureRl { get; }
+    public float BrakePressureRr { get; }
     public float BrakeTempFl { get; }
     public float BrakeTempFr { get; }
     public float BrakeTempRl { get; }
     public float BrakeTempRr { get; }
     public string CarId { get; }
+    public float Clutch { get; }
+    public float DiscLifeFl { get; }
+    public float DiscLifeFr { get; }
+    public float DiscLifeRl { get; }
+    public float DiscLifeRr { get; }
     public string DriverDisplayName { get; }
+    public string DriverFullName { get; }
+    public int FrontBrakeCompound { get; }
     public float Fuel { get; }
     public int Gear { get; }
+    public float Heading { get; }
     public bool IsEngineRunning { get; }
+    public bool IsIgnitionOn { get; }
     public bool IsInvalid { get; }
+    public bool IsPitLimiterOn { get; }
     public int LapTimeMs { get; }
+    public float LocationX { get; }
+    public float LocationY { get; }
     public float NormalisedCarPosition { get; }
+    public float PadLifeFl { get; }
+    public float PadLifeFr { get; }
+    public float PadLifeRl { get; }
+    public float PadLifeRr { get; }
+    public float Pitch { get; }
+    public int RearBrakeCompound { get; }
+    public float Roll { get; }
     public int Rpm { get; }
     public int SectorIndex { get; }
     public float SlipAngleFl { get; }
@@ -90,6 +139,7 @@ public class AccTelemetryFrame
     public DateTime TimeStamp { get; }
     public string TrackId { get; }
     public float TractionControl { get; }
+    public float TurboBoost { get; }
     public float TyreCoreTempFl { get; }
     public float TyreCoreTempFr { get; }
     public float TyreCoreTempRl { get; }
@@ -102,8 +152,11 @@ public class AccTelemetryFrame
     public float TyreTempFr { get; }
     public float TyreTempRl { get; }
     public float TyreTempRr { get; }
-    public float LocationX { get; set; }
-    public float LocationY { get; set; }
+    public float WaterTemp { get; }
+    public float WheelSlipFl { get; }
+    public float WheelSlipFr { get; }
+    public float WheelSlipRl { get; }
+    public float WheelSlipRr { get; }
 
     public override string ToString()
     {
