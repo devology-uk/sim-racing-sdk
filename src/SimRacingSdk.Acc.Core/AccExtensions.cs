723065fc4e4bd05a18e1bbf7ebf8c4db2870763e
Mike Hanson <mike@mikehanson.com>
2025-08-18 04:52:30 +0100

    Merged extension classes

--- src/SimRacingSdk.Acc.Core/AccExtensions.cs
+++ src/SimRacingSdk.Acc.Core/AccExtensions.cs
@@ -25,4 +25,25 @@ public static class AccExtensions
                    _ => sessionType.ToString()
                };
     }
+
+    public static string ToFriendlyName(this Nationality nationality)
+    {
+        return nationality switch
+        {
+            Nationality.ChineseTaipei => "Chinese Taipei",
+            Nationality.CzechRepublic => "Czech Republic",
+            Nationality.GreatBritain => "Great Britain",
+            Nationality.HongKong => "Hong Kong",
+            Nationality.NewCaledonia => "New Caledonia",
+            Nationality.NewZealand => "New Zealand",
+            Nationality.NorthernIreland => "Northern Ireland",
+            Nationality.PapuaNewGuinea => "Papua New Guinea",
+            Nationality.PuertoRico => "Puerto Rico",
+            Nationality.SanMarino => "San Marino",
+            Nationality.SaudiArabia => "Saudi Arabia",
+            Nationality.SouthAfrica => "South Africa",
+            Nationality.SouthKorea => "South Korea",
+            _ => nationality.ToString()
+        };
+    }
 }
