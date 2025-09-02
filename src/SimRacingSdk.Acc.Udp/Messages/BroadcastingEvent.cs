66a4b8e2c5590e55467e2ac71781efc9845d337d
Mike Hanson <mike@mikehanson.com>
2025-09-02 07:43:47 +0100

    Enabled internal changes of car data on broadcasting event

--- src/SimRacingSdk.Acc.Udp/Messages/BroadcastingEvent.cs
+++ src/SimRacingSdk.Acc.Udp/Messages/BroadcastingEvent.cs
@@ -7,7 +7,7 @@ namespace SimRacingSdk.Acc.Udp.Messages;
 public record BroadcastingEvent
 {
     public BroadcastingEventType BroadcastingEventType { get; init; }
-    public CarInfo CarData { get; init; }
+    public CarInfo CarData { get; internal set; }
     public int CarId { get; init; }
     public string Message { get; init; }
     public int TimeMs { get; init; }
