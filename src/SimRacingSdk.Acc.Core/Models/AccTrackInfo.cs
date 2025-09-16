ebe970463acae812ffaa969f2ec7de5587726d54
Mike Hanson <mike@mikehanson.com>
2025-09-16 10:40:31 +0100

    Add full name and renamed name to short name on acc track info

--- src/SimRacingSdk.Acc.Core/Models/AccTrackInfo.cs
+++ src/SimRacingSdk.Acc.Core/Models/AccTrackInfo.cs
@@ -6,9 +6,10 @@ public record AccTrackInfo
 {
     public int Corners { get; init; }
     public string CountryTag { get; init; }
+    public string FullName { get; init; }
     public double Latitude { get; init; }
     public int Length { get; init; }
     public double Longitude { get; init; }
-    public string Name { get; init; }
+    public string ShortName { get; init; }
     public string TrackId { get; init; }
 }
