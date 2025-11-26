3113ada6275e50c8b7575855c90428d8609bfedf
Mike Hanson <mike@mikehanson.com>
2025-11-26 06:26:24 +0000

    Added keybinding to export minmal lmu car data to csv file

--- src/SimRacingSdk.Ams2.Demo/CarExplorer/CarExplorerViewModel.cs
+++ src/SimRacingSdk.Ams2.Demo/CarExplorer/CarExplorerViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using SimRacingSdk.Ams2.Core.Abstractions;
 using SimRacingSdk.Ams2.Core.Models;
 
@@ -10,17 +13,50 @@ public partial class CarExplorerViewModel : ObservableObject
     private readonly IAms2CarInfoProvider carInfoProvider;
 
     [ObservableProperty]
-    private string selectedClass = string.Empty;
-
-    [ObservableProperty]
     private Ams2CarInfo? selectedCar = null;
 
-    public ObservableCollection<string> CarClasses { get; } = [];
-    public ObservableCollection<Ams2CarInfo> Cars { get; } = [];
+    [ObservableProperty]
+    private string selectedClass = string.Empty;
 
     public CarExplorerViewModel(IAms2CarInfoProvider carInfoProvider)
     {
         this.carInfoProvider = carInfoProvider;
+    }
+
+    public ObservableCollection<string> CarClasses { get; } = [];
+    public ObservableCollection<Ams2CarInfo> Cars { get; } = [];
+
+    [RelayCommand]
+    private void ExportCsv()
+    {
+        var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+            "ams2-cars.csv");
+
+        using var streamWriter = new StreamWriter(filePath, options: new FileStreamOptions
+        {
+            Access = FileAccess.Write,
+            Mode = FileMode.Create
+        });
+
+        foreach(var ams2CarInfo in this.carInfoProvider.GetCarInfos())
+        {
+            var carInfo = $"{ams2CarInfo.Class},{ams2CarInfo.Model},{ams2CarInfo.Model},2025,{ams2CarInfo.Manufacturer}";
+            streamWriter.WriteLine(carInfo);
+            streamWriter.Flush();
+        }
+    }
+
+    internal void Init()
+    {
+        foreach(var carClass in this.carInfoProvider.GetCarClasses())
+        {
+            this.CarClasses.Add(carClass);
+        }
+
+        if(this.CarClasses.Count > 0)
+        {
+            this.SelectedClass = this.CarClasses[0];
+        }
     }
 
     partial void OnSelectedClassChanged(string value)
@@ -35,19 +71,6 @@ public partial class CarExplorerViewModel : ObservableObject
         if(this.Cars.Count > 0)
         {
             this.SelectedCar = this.Cars[0];
-        }
-    }
-    
-    internal void Init()
-    {
-        foreach(var carClass in this.carInfoProvider.GetCarClasses())
-        {
-            this.CarClasses.Add(carClass);
-        }
-
-        if(this.CarClasses.Count > 0)
-        {
-            this.SelectedClass = this.CarClasses[0];
         }
     }
 }
