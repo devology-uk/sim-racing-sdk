b0363f05f730985c9d71ac677bb7ace8832592f9
Mike Hanson <mike@mikehanson.com>
2025-11-26 06:57:10 +0000

    Added key binding to export ams2 track and layout data to csv

--- src/SimRacingSdk.Ams2.Demo/TrackExplorer/TrackExplorerViewModel.cs
+++ src/SimRacingSdk.Ams2.Demo/TrackExplorer/TrackExplorerViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.ObjectModel;
+using System.IO;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using SimRacingSdk.Ams2.Core.Abstractions;
 using SimRacingSdk.Ams2.Core.Models;
 
@@ -39,6 +41,44 @@ namespace SimRacingSdk.Ams2.Demo.TrackExplorer
         partial void OnSelectedTrackNameChanged(string value)
         {
             this.SelectedTrack = this.trackInfoProvider.FindTrackByShortName(value);
+        }
+
+        [RelayCommand]
+        private void ExportCsv()
+        {
+            var tracksFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                "ams2-tracks.csv");
+            var trackLayoutsFilePath =
+                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                    "ams2-track-layouts.csv");
+
+            using var tracksStreamWriter = new StreamWriter(tracksFilePath,
+                options: new FileStreamOptions
+                {
+                    Access = FileAccess.Write,
+                    Mode = FileMode.Create
+                });
+            using var layoutsStreamWriter = new StreamWriter(trackLayoutsFilePath,
+                options: new FileStreamOptions
+                {
+                    Access = FileAccess.Write,
+                    Mode = FileMode.Create
+                });
+
+            foreach(var ams2TrackInfo in this.trackInfoProvider.GeTrackInfos())
+            {
+                var trackInfo =
+                    $"{ams2TrackInfo.ShortName},{ams2TrackInfo.FullName},{ams2TrackInfo.AltitudeM},{ams2TrackInfo.CountryCode},{ams2TrackInfo.CountryCode},{ams2TrackInfo.Latitude},{ams2TrackInfo.Longitude}";
+                tracksStreamWriter.WriteLine(trackInfo);
+                tracksStreamWriter.Flush();
+
+                foreach(var ams2TrackLayoutInfo in ams2TrackInfo.Layouts)
+                {
+                    var layoutInfo = $"{ams2TrackInfo.ShortName},{ams2TrackLayoutInfo.Ams2TrackId},{ams2TrackLayoutInfo.Name},{ams2TrackLayoutInfo.Corners},{ams2TrackLayoutInfo.Grade},{ams2TrackLayoutInfo.LengthKm},{ams2TrackLayoutInfo.MaxGridSize},{ams2TrackLayoutInfo.TrackType}";
+                    layoutsStreamWriter.WriteLine(layoutInfo);
+                    layoutsStreamWriter.Flush();
+                }
+            }
         }
     }
 }
